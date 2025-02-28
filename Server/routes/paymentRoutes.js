e3bfc0284ffb40c30286ab5d476951968c66bdb4
Harman Dhiman <harmandhimn6776@gmail.com>
2025-02-28 15:34:40 +0530

    checking branch

--- Server/routes/paymentRoutes.js
+++ Server/routes/paymentRoutes.js
@@ -4,9 +4,6 @@ const router = express.Router();
 
 router.post('/create-payment-intent', createPaymentIntent);
 router.post('/webhook', express.raw({ type: 'application/json' }), handleWebhook);
-
-
-// Route to handle payment charge after token and clientSecret are received
 router.post('/charge', chargePayment);  // New route for handling charge
 
 
